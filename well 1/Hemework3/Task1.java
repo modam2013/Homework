bbc86f8fa6587e40e44af86dac706e542f985bdd
antonina <grigorevna.antoninka@mail.ru>
2022-10-09 21:45:49 +0700

    Homework!

---
+++ well 1/Hemework3/Task1.java
@@ -0,0 +1,13 @@
+package Hemework3;
+
+public class Task1 {
+    public static void main(String[] args){
+        var dog   = 8;
+        var cat   = 3.6;
+        var paper = 763789;
+
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+    }
+}
