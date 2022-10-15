0982d51e1e9e1365c11922b7c6c534a4aa612c82
antonina <grigorevna.antoninka@mail.ru>
2022-10-15 22:42:50 +0700

    массивы 2

---
+++ well 1/Hemevork10/Task1.java
@@ -0,0 +1,26 @@
+package Hemevork10;
+
+public class Task1 {
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
+    public static void main(String[] args){
+        int[] arr = generateRandomArray();
+        int amounts=0;
+        for (int i = 0; i < arr.length; i++) {
+            amounts = amounts + arr[i];
+        }System.out.println  ( "Сумма трат за месяц составила  " +  amounts  + " рублей");
+
+
+
+
+
+
+    }
+
+}
---
+++ well 1/Hemevork10/Task2.java
@@ -0,0 +1,27 @@
+package Hemevork10;
+
+public class Task2 {
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = generateRandomArray();
+        int max = -1;
+        int min = 200_001;
+        for (int i = 0; i < arr.length; i++){
+            if ( arr[i] > max){
+                max = arr[i];
+            } if ( arr[i] < min ){
+                min = arr[i];
+            }
+        }
+        System.out.println( "Максимальная сумма трат за день составила  " + max + " рублей");
+        System.out.println( "Минимальная сумма трат за день составила  " +  min + " рублей");
+    }
+}
